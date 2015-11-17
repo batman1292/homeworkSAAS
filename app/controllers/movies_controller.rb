fc4f347cafa16386b3b1f87b0fe6ef31f1ba3cf0
batman1292 <batman12922@gmail.com>
2015-11-17 18:02:18 +0700

    4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,5 +8,13 @@ class MoviesController < ApplicationController
     #print id
     # will render app/views/movies/show.html.haml by default
   end
-
+  def new
+    # default: render 'new' template
+  end
+  # in movies_controller.rb
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
 end
