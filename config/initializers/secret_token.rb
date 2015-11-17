2074c91e5e8ca00d0a4e9ed35df31ebdc4f3a9ee
batman1292 <batman12922@gmail.com>
2015-11-17 14:12:39 +0700

    create SAAS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeworkSAAS::Application.config.secret_token = '6e96328ff751663d232b23ddf71942969c419a06b1b1981e9b318938ed9816e186de2be96e4d5733d8e6953d1162ca8d4d3e0999bf64c049bfe9a34247ba9305'
