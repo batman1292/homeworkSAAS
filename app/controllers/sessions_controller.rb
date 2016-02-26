6e298136d52a9095cd8bf8f2b71e8f9b30f34f74
choocku <checkypw@gmail.com>
2016-02-26 14:12:02 +0700

    Completed App

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,8 @@ class SessionsController < ApplicationController
   # user shouldn't have to be logged in before logging in!
   skip_before_filter :set_current_user
   def create
-    auth=request.env["omniauth.auth"]
-    user=Moviegoer.find_by_provider_and_uid(auth["provider"],auth["uid"]) ||
-      Moviegoer.create_with_omniauth(auth)
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     redirect_to movies_path
   end
